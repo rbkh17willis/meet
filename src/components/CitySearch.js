41f1b1ff618bca690c4ace03106b3986c181d31e
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-04-01 20:24:40 -0700

    Testing for NumberofEvents, EventList, App and fine tunning other tests

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,48 +1,51 @@
-// src/components/CitySearch.js
+import { useEffect, useState } from 'react';
 
-import { useState } from "react";
+const CitySearch = ({allLocations, setCurrentCity}) => {
+    const [showSuggestion, setShowSuggestion] = useState(false);
+    const [query, setQuery] = useState('');
+    const [suggestions, setSuggaestions] = useState([]);
 
-const CitySearch = ({ allLocations }) => {
-    const [showSuggestions, setShowSuggestions] = useState(false);
-    const [query, setQuery] = useState("");
-    const [suggestions, setSuggestions] = useState([]);
-    const handleInputChanged = (event) => {
+    const handleInputChange = (event) => {
         const value = event.target.value;
         const filteredLocations = allLocations ? allLocations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         }) : [];
-    
         setQuery(value);
-        setSuggestions(filteredLocations);
-      };
-      const handleItemClicked = (event) => {
-        const value = event.target.textContent;
-        setQuery(value);
-        setShowSuggestions(false); // to hide the list
-      };
-      return (
+        setSuggaestions(filteredLocations);
+       // setShowSuggestion(false);
+
+
+    }
+    const handleClick = (event) => {
+      const value = event.target.textContent;
+      setQuery(value);
+      setShowSuggestion(false);
+      setCurrentCity(value);
+    } 
+
+    useEffect(() => {
+      setSuggaestions(allLocations);
+    }, [`${allLocations}`]);
+    return(
         <div id="city-search">
           <input
-            type="text"
-            className="city"
-            placeholder="Search for a city"
-            value={query}
-            onFocus={() => setShowSuggestions(true)}
-            onChange={handleInputChanged}
-          />
-          {showSuggestions ?
-            <ul className="suggestions">
-              {suggestions.map((suggestion) => {
-                return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
-              })}
-              <li key='See all cities' onClick={handleItemClicked}>
-                <b>See all cities</b>
-              </li>
-            </ul>
-            : null
-          }
+             type="text"
+             className="city"
+             placeholder="Search for a city"
+             value={query}
+             onFocus={() => setShowSuggestion(true)}
+             onChange={handleInputChange}
+         />  
+         {showSuggestion ? <ul className='suggestion'>
+            {suggestions.map((suggestion) => {
+                return <li onClick={handleClick} key={suggestion}>{suggestion}</li>
+            })}
+            <li key='See all the cities'> 
+              <b>See all cities</b>
+            </li>
+         </ul> : null}
         </div>
-     )
+    )
 }
 
 export default CitySearch;
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,24 @@
-// src/components/Event.js
+import { useState } from "react";
 
-const Event = () => {
+
+const  Event = ({event}) => {
+   const [showDetails, setShowDetails] = useState(false);
     return (
-      <li></li>
-    );
-  }
+        <li className="event">
+            <h2>{event.summary}</h2>
+            <p>{event.created}</p>
+            <p>{event.location}</p>
+            <button className='showDetailsButton' 
+            onClick={() => setShowDetails(!showDetails)}>
+                {showDetails ? 'Hide Details' : 'Show Details'}
+            </button>
+            {showDetails ? (
+                        <div className="details">
+                        <h3>Event Details</h3>
+                        <p>{event.details}</p>
+                        </div>) : null}
+        </li>
+    )
+}
 
 export default Event;
